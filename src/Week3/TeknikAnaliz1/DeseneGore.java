e101ff4b474b4ea2a2f5cd88e6a0e6deeca01997
SifirGibi <yazilim@sifirgibimakine.com>
2024-04-26 17:44:26 +0300

    DeseneGore

---
+++ src/Week3/TeknikAnaliz1/DeseneGore.java
@@ -0,0 +1,28 @@
+package Week3.TeknikAnaliz1;
+
+import java.util.Scanner;
+
+public class DeseneGore {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen n değerini girin: ");
+        int kullaniciGirdisi = input.nextInt();
+        int islemDegeri = recursiveIslemEksi(kullaniciGirdisi);
+        recursiveIslemArti(islemDegeri, kullaniciGirdisi);
+    }
+
+    // Subtract until 0 or minus int and printing result
+    public static int recursiveIslemEksi(int islemDegeri){
+        if(islemDegeri <=0) return islemDegeri;
+        System.out.print(islemDegeri +" ");
+        return recursiveIslemEksi(islemDegeri-5);
+    }
+
+
+    public static void recursiveIslemArti(int islemDegeri, int userInput){
+        if (userInput>=islemDegeri) {
+            System.out.print(islemDegeri + " ");
+            recursiveIslemArti(islemDegeri + 5, userInput);
+        }
+    }
+}
